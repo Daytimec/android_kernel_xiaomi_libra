rlawoehd187/kernel_lge_bullhead

ASoC: wcd9330: playback in higher quality
Newer codecs default to a "HIFI" mode playback, so let's do the best we
can here by initiliasing as an AB codec and enabling UHQA for all HiFi
rates, not just 24/192.

Signed-off-by: Laster K. (lazerl0rd) <officiallazerl0rd@gmail.com>

sound/soc/codecs/wcd9330.c
@@ -119,7 +119,7 @@ MODULE_PARM_DESC(cpe_debug_mode, "boot cpe in debug mode");
 
 static atomic_t kp_tomtom_priv;
 
-static int high_perf_mode;
+static int high_perf_mode = 1;
 module_param(high_perf_mode, int,
 			S_IRUGO | S_IWUSR | S_IWGRP);
 MODULE_PARM_DESC(high_perf_mode, "enable/disable class AB config for hph");
@@ -742,14 +742,27 @@ static int tomtom_compare_bit_format(struct snd_soc_codec *codec,
 	return ret;
 }
 
+static bool tomtom_bit_format_greater_eq(struct snd_soc_codec *codec,
+					 int bit_format)
+{
+	struct tomtom_priv *tomtom_p = snd_soc_codec_get_drvdata(codec);
+	int i;
+
+	for (i = 0; i < NUM_CODEC_DAIS; i++) {
+		if (tomtom_p->dai[i].bit_width >= bit_format)
+			return true;
+	}
+	return false;
+}
+
 static int tomtom_update_uhqa_mode(struct snd_soc_codec *codec, int path)
 {
 	int ret = 0;
 	struct tomtom_priv *tomtom_p = snd_soc_codec_get_drvdata(codec);
 
-	/* UHQA path has fs=192KHz & bit=24 bit */
-	if (((tomtom_get_sample_rate(codec, path) & 0xE0) == 0xA0) &&
-		(tomtom_compare_bit_format(codec, 24))) {
+	/* UHQA path for fs>48KHz & bit>=24 bit */
+	if (tomtom_get_sample_rate(codec, path) > 48 ||
+		tomtom_bit_format_greater_eq(codec, 24)) {
 		tomtom_p->uhqa_mode = 1;
 	} else {
 		tomtom_p->uhqa_mode = 0;

Daytimec/android_kernel_xiaomi_libra

Co-authored-by: mortezadadgar <mortezadadgar97@gmail.com>

sound/soc/codecs/wcd9330.c
@@ -90,7 +90,7 @@ MODULE_PARM_DESC(cpe_debug_mode, "boot cpe in debug mode");
 
 static atomic_t kp_tomtom_priv;
 
-static int high_perf_mode;
+static int high_perf_mode = 1;
 module_param(high_perf_mode, int,
 			S_IRUGO | S_IWUSR | S_IWGRP);
 MODULE_PARM_DESC(high_perf_mode, "enable/disable class AB config for hph");
@@ -719,9 +719,9 @@ static int tomtom_update_uhqa_mode(struct snd_soc_codec *codec, int path)
 	int ret = 0;
 	struct tomtom_priv *tomtom_p = snd_soc_codec_get_drvdata(codec);
 
-	/* UHQA path has fs=192KHz & bit=24 bit */
-	if (((tomtom_get_sample_rate(codec, path) & 0xE0) == 0xA0) &&
-		(tomtom_compare_bit_format(codec, 24))) {
+	/* UHQA path for fs>48KHz & bit>=24 bit */
+	if (tomtom_get_sample_rate(codec, path) > 48 ||
+		tomtom_compare_bit_format(codec, 24)) {
 		tomtom_p->uhqa_mode = 1;
 	} else {
 		tomtom_p->uhqa_mode = 0;